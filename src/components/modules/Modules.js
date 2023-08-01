2bc9b5240f627807cd0213ffa1b6bc7e28fbc6b2
Lin1404 <78375725+Lin1404@users.noreply.github.com>
2023-08-01 13:29:17 -0400

    Update Modules.js

--- src/components/modules/Modules.js
+++ src/components/modules/Modules.js
@@ -2,12 +2,12 @@ import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const modulesRouting = [
-  { path: "/tns/showcases", label: "Show Cases" },
-  { path: "/tns/workshop", label: "Our Workshop" },
-  { path: "/tns/certifications", label: "Certifications" },
   { path: "/tns/drp", label: "DRP Partnerwith" },
+  { path: "/tns/showcases", label: "Before and After Photos" },
+  { path: "/tns/team", label: "Our Team" },
+  { path: "/tns/certifications", label: "Certifications" },
 ];
-// https://www.tnsautoinc.com/
+
 export default function Modules() {
   return (
     <div className="modulesSection">
