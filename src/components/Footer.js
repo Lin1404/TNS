d98bab7d89ba05a08ea04c8dba4cfd30d69440ab
Lin1404 <78375725+Lin1404@users.noreply.github.com>
2023-08-09 15:37:18 -0400

    change "time" to "hour" in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -30,7 +30,7 @@ export default function Footer() {
           <div className="info">
             <h3>Contact us:</h3>
             <div className="infoDetail">
-              <h4>Time</h4>
+              <h4>Hour</h4>
               <div>Monday - Friday 7AM - 5PM</div>
               <div>Saturday 7AM - 3PM</div>
             </div>
