d1ec5e2a3902a3960f41fc7e8ac9b527117b87e7
Lin1404 <hl4279@nyu.edu>
2023-09-13 14:49:51 -0400

    connect to emailjs

--- src/components/message/MessageWindow.js
+++ src/components/message/MessageWindow.js
@@ -1,23 +1,46 @@
 import {
   Box,
   Button,
-  Container,
   FormControl,
   Grid,
   Stack,
   TextField,
 } from "@mui/material";
 import { Form } from "react-router-dom";
-import axios from "axios";
 import emailjs from "@emailjs/browser";
+import { useState } from "react";
+import { MuiTelInput } from "mui-tel-input";
 
 const SERVICE_ID = "service_rkvpp6n";
 const TEMPLATE_ID = "template_e06xv0o";
 const USER_ID = "r4Jj97hnasFu2RRk8";
 
 export default function MessageWindow() {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    model: "",
+    message: "",
+  });
+  const [isOnSubmit, setIsOnSubmit] = useState(false);
   const handleOnSubmit = (e) => {
-    console.log(e);
+    if (
+      formValues.name !== "" &&
+      formValues.phone.length === 12 &&
+      formValues.email !== "" &&
+      formValues.message !== ""
+    ) {
+      emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        }
+      );
+      e.target.reset();
+    }
   };
 
   return (
@@ -28,50 +51,88 @@ export default function MessageWindow() {
     >
       <h3>Contact Us:</h3>
       <FormControl>
-        <Stack spacing={3} padding={3}>
-          <Grid container>
-            <Grid xs={6} paddingRight={4}>
-              <TextField id="name" label="Name" name="user_name" required />
+        <Form onSubmit={handleOnSubmit}>
+          <Stack spacing={3} padding={3}>
+            <Grid container>
+              <Grid xs={6} paddingRight={4}>
+                <TextField
+                  id="name"
+                  label="Name"
+                  name="name"
+                  required
+                  onChange={(e) =>
+                    setFormValues({ ...formValues, name: e.target.value })
+                  }
+                  error={isOnSubmit && formValues.name === ""}
+                />
+              </Grid>
+              <Grid xs={6} textAlign={"right"}>
+                <TextField
+                  id="model"
+                  label="Car Model (Optional)"
+                  name="model"
+                  onChange={(e) =>
+                    setFormValues({ ...formValues, model: e.target.value })
+                  }
+                />
+              </Grid>
             </Grid>
-            <Grid xs={6} textAlign={"right"}>
-              <TextField
-                id="modal"
-                label="Car Modal (Optional)"
-                name="car_modal"
-                required
-              />
+            <Grid container>
+              <Grid xs={6} paddingRight={4}>
+                <MuiTelInput
+                  id="phone"
+                  label="Phone Number"
+                  name="phone"
+                  required
+                  defaultCountry="US"
+                  forceCallingCode="1"
+                  onlyCountries={["US"]}
+                  disableDropdown
+                  disableFormatting
+                  value={formValues.phone}
+                  onChange={(value) =>
+                    value.length < 13 &&
+                    setFormValues({ ...formValues, phone: value })
+                  }
+                  error={isOnSubmit && formValues.phone.length !== 12}
+                />
+              </Grid>
+              <Grid xs={6} textAlign={"right"}>
+                <TextField
+                  id="email"
+                  label="E-mail"
+                  name="email"
+                  required
+                  error={isOnSubmit && formValues.email === ""}
+                  onChange={(e) =>
+                    setFormValues({ ...formValues, email: e.target.value })
+                  }
+                />
+              </Grid>
             </Grid>
-          </Grid>
-          <Grid container>
-            <Grid xs={6} paddingRight={4}>
-              <TextField
-                id="phone"
-                label="Phone Number"
-                name="user_phone"
-                required
-              />
-            </Grid>
-            <Grid xs={6} textAlign={"right"}>
-              <TextField id="email" label="E-mail" name="user_email" required />
-            </Grid>
-          </Grid>
-          <TextField
-            id="message"
-            label="Message"
-            name="message"
-            multiline
-            rows={6}
-            // placeholder="Please Enter Your Email"
-            required
-          />
-          <Button
-            onClick={() => handleOnSubmit()}
-            variant="contained"
-            sx={{ width: "50%", alignSelf: "center" }}
-          >
-            Submit
-          </Button>
-        </Stack>
+            <TextField
+              id="message"
+              label="Message"
+              name="message"
+              multiline
+              required
+              rows={6}
+              helperText="We Love to Hear Your Feedback!"
+              error={isOnSubmit && formValues.message === ""}
+              onChange={(e) =>
+                setFormValues({ ...formValues, message: e.target.value })
+              }
+            />
+            <Button
+              type="submit"
+              onClick={() => setIsOnSubmit(true)}
+              variant="contained"
+              sx={{ width: "50%", alignSelf: "center" }}
+            >
+              SEND MESSAGE
+            </Button>
+          </Stack>
+        </Form>
       </FormControl>
     </Box>
   );
