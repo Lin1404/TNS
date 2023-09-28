a59c81074b7dd8aa54fb70ff911ac4295777ac24
Haohui Lin <hl4279@nyu.edu>
2023-09-28 13:46:12 -0400

    add manu in module (#6)

--- src/components/modules/Header.js
+++ src/components/modules/Header.js
@@ -3,48 +3,171 @@ import "../style.css";
 import Modules from "../Modules";
 import { Link } from "react-router-dom";
 import useWindowDimensions from "../useWindowDimensions";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
 
 export default function Header() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   const { width } = useWindowDimensions();
-  return width > 950 ? (
-    <div className="header backgroundColor">
-      <div className="headerGroup">
-        <Link to={"/tnsauto"}>
-          <img
-            src={tnsLogo}
-            alt="TNS logo"
-            style={{ width: "5rem", paddingRight: "1rem" }}
-          />
-        </Link>
-        <div className="title">
-          <h1
-            style={{ marginBottom: "5%", fontSize: "40px", color: "#1b73bc" }}
+
+  return (
+    <>
+      {width > 950 ? (
+        <div className="header backgroundColor">
+          <div className="headerGroup">
+            <Link to={"/tnsauto"}>
+              <img
+                src={tnsLogo}
+                alt="TNS logo"
+                style={{ width: "5rem", paddingRight: "1rem" }}
+              />
+            </Link>
+            <div className="title">
+              <h1
+                style={{
+                  marginBottom: "5%",
+                  fontSize: "40px",
+                  color: "#1b73bc",
+                }}
+              >
+                TNS Auto Collision
+              </h1>
+            </div>
+          </div>
+          <Button
+            className="proposalButton"
+            onClick={() => setIsDialogOpen(true)}
           >
-            TNS Auto Collision
-          </h1>
+            Proposal
+          </Button>
+          <Modules />
         </div>
-      </div>
-      <Modules />
-    </div>
-  ) : (
-    <div className="header backgroundColor">
-      <div className="headerGroup">
-        <Link to={"/tnsauto"}>
-          <img
-            src={tnsLogo}
-            alt="TNS logo"
-            style={{ width: "5rem", paddingRight: "1rem" }}
+      ) : (
+        <div className="header backgroundColor">
+          <div className="headerGroup">
+            <Link to={"/tnsauto"}>
+              <img
+                src={tnsLogo}
+                alt="TNS logo"
+                style={{ width: "5rem", paddingRight: "1rem" }}
+              />
+            </Link>
+            <div className="title">
+              <h1
+                style={{
+                  marginBottom: "5%",
+                  fontSize: "40px",
+                  color: "#1b73bc",
+                }}
+              >
+                TNS Auto Collision
+              </h1>
+            </div>
+          </div>
+          <Modules isButton={true} />
+        </div>
+      )}
+
+      <Dialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
+        <DialogTitle>Please Enter Parner Code:</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To See the TNS Proposal, please enter the parner code:
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="code"
+            label="Parner Code"
+            type="coode"
+            fullWidth
+            variant="standard"
           />
-        </Link>
-        <div className="title">
-          <h1
-            style={{ marginBottom: "5%", fontSize: "40px", color: "#1b73bc" }}
-          >
-            TNS Auto Collision
-          </h1>
-        </div>
-      </div>
-      <Modules isButton={true} />
-    </div>
+          <DialogActions>
+            <Button onClick={() => setIsDialogOpen(false)}>Cancel</Button>
+            <Button onClick={() => setIsDialogOpen(false)}>Submit</Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+    </>
   );
+
+  // <>
+  // width > 950 ? (
+
+  //   <div className="header backgroundColor">
+  //     <div className="headerGroup">
+  //       <Link to={"/tnsauto"}>
+  //         <img
+  //           src={tnsLogo}
+  //           alt="TNS logo"
+  //           style={{ width: "5rem", paddingRight: "1rem" }}
+  //         />
+  //       </Link>
+  //       <div className="title">
+  //         <h1
+  //           style={{ marginBottom: "5%", fontSize: "40px", color: "#1b73bc" }}
+  //         >
+  //           TNS Auto Collision
+  //         </h1>
+  //       </div>
+  //     </div>
+  //     <Button className="proposalButton" onClick={() => setIsDialogOpen(true)}>
+  //       Proposal
+  //     </Button>
+  //     <Modules />
+  //   </div>
+  // ) : (
+  //   <div className="header backgroundColor">
+  //     <div className="headerGroup">
+  //       <Link to={"/tnsauto"}>
+  //         <img
+  //           src={tnsLogo}
+  //           alt="TNS logo"
+  //           style={{ width: "5rem", paddingRight: "1rem" }}
+  //         />
+  //       </Link>
+  //       <div className="title">
+  //         <h1
+  //           style={{ marginBottom: "5%", fontSize: "40px", color: "#1b73bc" }}
+  //         >
+  //           TNS Auto Collision
+  //         </h1>
+  //       </div>
+  //     </div>
+  //     <Modules isButton={true} />
+  //   </div>
+  // )
+  // <Dialog open={isDialogOpen} onClose={()=>setIsDialogOpen(false)}>
+  // <DialogTitle>Please Enter Parner Code:</DialogTitle>
+  // <DialogContent>
+  //   <DialogContentText>
+  //     To See the TNS Proposal, please enter the parner code.
+  //   </DialogContentText>
+  //   <TextField
+  //   autoFocus
+  //   margin="dense"
+  //         id="code"
+  //         label="Parner Code"
+  //         type="coode"
+  //         fullWidth
+  //         variant="standard"
+  //   />
+  //             <DialogActions>
+  //       <Button onClick={()=>setIsDialogOpen(false)}>Cancel</Button>
+  //       <Button onClick={()=>setIsDialogOpen(false)}>Submit</Button>
+  //     </DialogActions>
+
+  // </DialogContent>
+  // </Dialog>
+  // </>
+  // )
 }
