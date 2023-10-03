43b46b136f2530e3d22bf850051c3feadb65b80f
Lin1404 <hl4279@nyu.edu>
2023-10-03 14:42:23 -0400

    add facility image

--- src/components/modules/Facility.js
+++ src/components/modules/Facility.js
@@ -1,5 +1,12 @@
 import Header from "./Header";
 import officeImg from "../../assets/Office.jpeg";
+import facility1 from "../../assets/facility1.jpg";
+import facility2 from "../../assets/facility2.jpg";
+import facility3 from "../../assets/facility3.jpg";
+import facility4 from "../../assets/facility4.jpg";
+import { Grid } from "@mui/material";
+
+const facilities = [facility3, facility1, facility2];
 
 export default function Facility() {
   return (
@@ -7,6 +14,19 @@ export default function Facility() {
       <Header />
       <h2 className="sectionTitle">Facility:</h2>
       <img src={officeImg} alt="Office Img" style={{ width: "80%" }} />
+      <img src={facility4} alt="Facility Img" style={{ width: "80%" }} />
+
+      <Grid container sx={{ padding: "0 3%" }}>
+        {facilities.map((item) => (
+          <Grid xs={6}>
+            <img
+              src={item}
+              alt="Facility Img"
+              style={{ width: "90%", padding: "2%" }}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
