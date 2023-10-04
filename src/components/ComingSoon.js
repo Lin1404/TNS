2e2f36a87b5defb1d511ac1d63c432411f781f6d
Lin1404 <hl4279@nyu.edu>
2023-10-04 10:20:19 -0400

    add portfolio page

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,13 +1,11 @@
 import { Button, Typography } from "@mui/material";
-import Header from "./modules/Header";
 import { Link } from "react-router-dom";
 
 export default function ComingSoon() {
   return (
     <>
-      <Header />
       <div className="moduleContainer">
-        <h2 className="sectionTitle">Coming Soon...</h2>
+        <h2 style={{ fontFamily: "cursive" }}>Coming Soon...</h2>
         <Link to={"/tnsauto"}>
           <Button>
             <Typography>Click here back to Home page.</Typography>
