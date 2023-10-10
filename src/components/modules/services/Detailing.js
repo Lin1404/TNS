b2c019336dc0ad986431d62cd7a0c6fe1c5631ce
Lin1404 <hl4279@nyu.edu>
2023-10-10 16:47:48 -0400

    enhance detailing context

--- src/components/modules/services/Detailing.js
+++ src/components/modules/services/Detailing.js
@@ -17,20 +17,17 @@ export default function Detailing() {
             </Typography>
             <Divider variant="middle" />
             <Typography variant="subtitle1" padding={2}>
-              Welcome to TNS Auto Collision, your premium destination for
-              comprehensive auto collision repairs. Committed to excellence, our
-              full-service center guarantees top-notch repair work on every
-              vehicle we service. Our highly skilled technicians are proficient
-              in utilizing our state-of-the-art equipment, including a
-              computerized laser frame measuring system for precise frame work,
-              advanced welding equipment, specialized tools for aluminum repair,
-              and high-quality paint equipment.
+              Welcome to TNS Auto Collision, where we treat every vehicle with
+              expert care, as if it were our own. Our skilled technicians
+              meticulously inspect and polish repaired areas, ensuring they look
+              flawless leaving no trace of the incident.
             </Typography>
             <Typography variant="subtitle1" padding={2}>
-              Each vehicle entrusted to us undergoes a meticulous assessment,
-              enabling us to create a customized work plan aimed at restoring
-              your vehicle to its pre-loss condition. Experience unparalleled
-              expertise and attention to detail at TNS Auto Collision.
+              When you arrive to collect your vehicle, you will find it
+              impeccably washed and meticulously wiped down, delivering an
+              experience akin to driving away in a showroom-condition, brand-new
+              car. At TNS Auto Collision, expect nothing less than perfection in
+              every detail.
             </Typography>
           </Stack>
         </Grid>
