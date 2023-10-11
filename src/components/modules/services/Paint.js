0bd55e9bc4238f7ff79ebb7273905727bab10f97
Lin1404 <hl4279@nyu.edu>
2023-10-11 16:26:04 -0400

    enhance paint page

--- src/components/modules/services/Paint.js
+++ src/components/modules/services/Paint.js
@@ -1,11 +1,60 @@
 import Header from "../Header";
 import ppg2 from "../../../assets/ppg2.jpeg";
+import { Divider, Grid, Stack } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import paintCover from "../../../assets/paintCover.jpg";
+import paint from "../../../assets/paint.jpg";
 import ecoFriendly from "../../../assets/ecoFriendly.png";
 
 export default function Paint() {
   return (
     <>
       <Header />
+      <div className="serviceBackgroundColor">
+        <img src={paintCover} alt="repair Img" className="ImgFill" />
+        <Grid container xs={12} className="highlight">
+          <Grid item xs={5}>
+            <Stack className="serviceContext">
+              <Typography variant="h3" padding={1}>
+                EXPERT ESTIMATING
+              </Typography>
+              <Divider variant="middle" />
+              <Typography variant="subtitle1" padding={2}>
+                At TNS Auto Collision, our experienced estimators conduct a
+                meticulous assessment of your vehicle's damage, providing you
+                with a comprehensive report. Our process begins with a thorough
+                visual inspection to compile a detailed list of necessary
+                repairs, aiming to restore your vehicle to its pre-loss
+                condition. Utilizing our advanced computerized estimating
+                system, we research the correct parts, labor, and safety
+                procedures tailored to your specific vehicle.
+              </Typography>
+              <Typography variant="subtitle1" padding={2}>
+                If you are filing an insurance claim, we collaborate closely
+                with both you and your insurance company. We schedule
+                appointments at our shop, facilitating full disassembly of
+                impacted areas. This approach enables our team and the insurance
+                adjuster to assess required parts accurately, eliminating any
+                delays in the repair process.
+              </Typography>
+              <Typography variant="subtitle1" padding={2}>
+                It's essential to remember that you have the right to choose any
+                licensed repair shop for your vehicle's inspection and repair.
+                At TNS Auto Collision, our dedicated staff is committed to
+                simplifying the estimating and repair process for our valued
+                customers. Your satisfaction and convenience are our priorities.
+              </Typography>
+            </Stack>
+          </Grid>
+          <Grid item xs={5}>
+            <img
+              src={paint}
+              alt="Aluminum Certified"
+              style={{ width: "90%" }}
+            />
+          </Grid>
+        </Grid>
+      </div>
       <h2 className="sectionTitle">Our Best Color Result</h2>
       <div className="ppgAnnouncementContainer">
         <img src={ecoFriendly} alt="illustration" style={{ width: "25%" }} />
