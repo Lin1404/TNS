250c82d561fddeead0f723b4380ddf9d77593ae3
Haohui Lin <hl4279@nyu.edu>
2023-10-19 10:23:24 -0400

    Update facility photos round1 (#28)
    * create tabs modules
    
    * styling
    
    * add tabs in home page
    
    * update before and after photos
    
    * update office photos
    
    * update shop photos
    
    * update facility page

--- src/components/modules/Facility.js
+++ src/components/modules/Facility.js
@@ -1,24 +1,33 @@
 import Header from "./Header";
-import officeImg from "../../assets/Office.jpeg";
-import office2 from "../../assets/Office2.png";
-import facility1 from "../../assets/facility1.jpg";
-import facility2 from "../../assets/facility2.jpg";
-import facility3 from "../../assets/facility3.jpg";
-import facility4 from "../../assets/facility4.jpg";
-import facility5 from "../../assets/facility5.jpg";
-import facility6 from "../../assets/facility6.jpg";
-import facility7 from "../../assets/facility7.jpg";
-import facility8 from "../../assets/facility8.jpg";
+import officeImg from "../../assets/office.jpeg";
+import office2 from "../../assets/office2.png";
+import shop1 from "../../assets/shop1.jpg";
+import shop2 from "../../assets/shop2.jpg";
+import shop3 from "../../assets/shop3.jpg";
+import shop4 from "../../assets/shop4.jpg";
+import shop5 from "../../assets/shop5.jpg";
+import shop6 from "../../assets/shop6.jpg";
+import shop7 from "../../assets/shop7.jpg";
+import office3 from "../../assets/office3.jpg";
+import office4 from "../../assets/office4.jpg";
+import office5 from "../../assets/office5.jpg";
+import office6 from "../../assets/office6.jpg";
+import office7 from "../../assets/office7.jpg";
 import { Grid } from "@mui/material";
 
 const facilities = [
-  facility3,
-  facility2,
-  facility8,
-  facility1,
-  facility6,
-  facility5,
-  facility7,
+  office3,
+  office4,
+  office5,
+  office6,
+  office7,
+  shop1,
+  shop2,
+  shop3,
+  shop4,
+  shop5,
+  shop6,
+  shop7,
 ];
 
 export default function Facility() {
@@ -37,7 +46,7 @@ export default function Facility() {
         style={{ width: "80%", paddingBottom: "1%" }}
       />
       <img
-        src={facility4}
+        src={office3}
         alt="Office Img"
         style={{ width: "80%", paddingBottom: "1%" }}
       />
