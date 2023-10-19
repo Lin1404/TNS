236015a335ae4e42d28a66646d283cf1cd68adf2
Haohui Lin <hl4279@nyu.edu>
2023-10-19 16:09:03 -0400

    Update before and after photos round1 (#29)
    * add photos
    
    * update close icon

--- src/components/modules/BeforeAndAfter.js
+++ src/components/modules/BeforeAndAfter.js
@@ -6,9 +6,14 @@ import compare2 from "../../assets/compare2.png";
 import compare3 from "../../assets/compare3.png";
 import compare4 from "../../assets/compare4.png";
 import compare5 from "../../assets/compare5.png";
-// import compare6 from "../../assets/compare6.png";
+import compare6 from "../../assets/compare6.png";
 import compare7 from "../../assets/compare7.png";
+import compare8 from "../../assets/compare8.png";
+import compare9 from "../../assets/compare9.png";
+import compare10 from "../../assets/compare10.png";
+import compare11 from "../../assets/compare11.png";
 import { Button, Modal } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 const imgsToCompare = [
   compare1,
@@ -16,8 +21,12 @@ const imgsToCompare = [
   compare3,
   compare4,
   compare5,
-  // compare6,
+  compare6,
   compare7,
+  compare8,
+  compare9,
+  compare10,
+  compare11,
 ];
 
 const style = {
@@ -25,7 +34,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: "60%",
+  width: "50%",
   bgcolor: "#cae4fd",
   boxShadow: 24,
   p: 4,
@@ -80,12 +89,12 @@ export default function BeforeAndAfter() {
                 onClick={handleClose}
                 className="galleryModalButton"
               >
-                X
+                <CloseIcon />
               </button>
             </div>
             <Carousel
               style={{
-                width: "60%",
+                width: "40%",
                 marginLeft: "25%",
               }}
               imgs={imgsToCompare}
